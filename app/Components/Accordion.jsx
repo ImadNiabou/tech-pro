91bb1d30c08d916c874676fd60ed8dce888fc725
Imad Niabou <imadniabou@gmail.com>
2024-04-10 12:47:14 +0200

    first commit

--- app/Components/Accordion.jsx
+++ app/Components/Accordion.jsx
@@ -1,9 +1,10 @@
 "use client";
 import * as React from "react";
+import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { fadeIn } from "../variants";
 import { motion } from "framer-motion";
@@ -16,23 +17,18 @@ export default function AccordionExpandDefault() {
       whileInView={"show"}
       className="max-w-7xl mx-auto mt-18"
     >
-      <div className="my-16">
-        <Accordion defaultExpanded>
+      <div>
+        <Accordion>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
             id="panel1-header"
           >
-            <Typography className="text-gray-600">
-              Expanded by default
-            </Typography>
+            Question 1
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography className="text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+          <AccordionDetails className="rounded-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
           </AccordionDetails>
         </Accordion>
         <Accordion>
@@ -41,31 +37,26 @@ export default function AccordionExpandDefault() {
             aria-controls="panel2-content"
             id="panel2-header"
           >
-            <Typography className="text-gray-600">Header</Typography>
+            Question 2
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography className="text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+          <AccordionDetails className="rounded-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
           </AccordionDetails>
         </Accordion>
-        <Accordion>
+        <Accordion defaultExpanded>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2-content"
-            id="panel2-header"
+            aria-controls="panel3-content"
+            id="panel3-header"
           >
-            <Typography className="text-gray-600">Header</Typography>
+            Question 3
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography className="text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+          <AccordionDetails className="rounded-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
           </AccordionDetails>
+          <AccordionActions></AccordionActions>
         </Accordion>
       </div>
     </motion.div>
